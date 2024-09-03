381c0d680e2ae1a707af9fa59e5a70cfb3ecb1f6
Letícia Pereira <lettynunes01@gmail.com>
2024-09-03 15:33:41 -0300

    atualizando

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import authRouter from './routers/authRouter.js'
 import accountRouter from './routers/accountRouter.js'
+import { ENVIRONMENT,PORT, HOST } from './config.js'
+
 
 const app = express()
 
 app.use('/auth', authRouter)
 app.use('/account', accountRouter)
 
-app.listen(3000, () => {
-    console.log('Servidor Rodando em http://localhost:3000')
+
+app.listen(PORT, () => {
+    console.log(`Servidor Rodando no ambiente ${ENVIRONMENT} em ${ ENVIRONMENT == 'production' ? HOST : HOST+':'+PORT }`)
 })
